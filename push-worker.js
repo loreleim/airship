380a2e950f5222f13c136a5af1f5a2cdc135ed48
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-02-24 21:49:02 -0800

    dis workies

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1614230028.870804
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://www.oregonlottery.org/assets/wp\u002Dcontent/themes/oregonlottery20xx/library/img/favicon/apple\u002Dicon\u002D180x180.png',
+  defaultTitle: 'Oregon Lottery',
+  defaultActionURL: 'http://localhost:8888/experimental',
+  appKey: '6ZTNl9LTSCeWZ7hlTP7hmQ',
+  token: 'MTo2WlRObDlMVFNDZVdaN2hsVFA3aG1ROm0yR0ZnOEhrTlF5X1BsWTRUVWxkLWVZQUxheWdDa2ZXOVp6eVBzLVFmbkU',
+  vapidPublicKey: 'BNFpssqF46xwv3UEcZgyWcMY8uro_MXFyAPPd5LVuMmlTGAsddwfvjZGcZ80O0p5mgniitjnUY3wRvxFsdBlxRE='
+})
